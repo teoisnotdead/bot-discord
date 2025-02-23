fb3f4e45dd41ed64b062b12e9384d0a65eaec992
TEO <alfredo.ssm@gmail.com>
2025-02-23 16:52:20 -0300

    se cambia import

--- services/instagramService.js
+++ services/instagramService.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const { isNewPost, updateStorage } = require("./storageService");
 
 const INSTAGRAM_USERNAME = process.env.INSTAGRAM_USERNAME;
--- services/tiktokService.js
+++ services/tiktokService.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const { isNewPost, updateStorage } = require("./storageService");
 
 const TIKTOK_USERNAME = process.env.TIKTOK_USERNAME;
